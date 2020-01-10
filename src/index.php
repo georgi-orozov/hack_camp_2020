5beef612452cb2a3418d78c14724bc3d8d0ae807
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-10 17:48:43 +0000

    Getting data from the API

--- src/index.php
+++ src/index.php
@@ -72,4 +72,41 @@ if(isset($_POST['logout'])) {
 // redirect to index.php
     header('location: index.php');
 }
+
+function callAPI($method, $url, $data){
+    $curl = curl_init();
+    switch ($method){
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+
+        'Content-Type: application/json',
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
+    curl_close($curl);
+    return $result;
+}
+$get_data = callAPI('GET', 'http://3.11.108.65/api/v1/utilities/', false);
+
+$response = json_decode($get_data, true);
+var_dump($response);
+die();
 require_once('Views/index.phtml');
