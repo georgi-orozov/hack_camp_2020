a09be5379224c6ceae31849dab3fbb417dd51fab
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-21 14:37:57 +0000

    UserFunctions testing finished

--- src/Models/UserFunctionsTest.php
+++ src/Models/UserFunctionsTest.php
@@ -4,9 +4,26 @@ use PHPUnit\Framework\TestCase;
 
 class UserFunctionsTest extends TestCase {
 
-    public function testLogin() {
+    public function testLoginSuccess() {
         $userFunctions = new UserFunctions();
         $result = $userFunctions->login("name@email.com", "name");
         $this->assertSame(true, $result);
     }
+
+    public function testLoginFailure() {
+        $userFunctions = new UserFunctions();
+        $result = $userFunctions->login("exampla@e-mail.com", "nopassword");
+        $this->assertSame(false, $result);
+    }
+
+    public function testRegistrationSuccess() {
+        $userFunctions = new UserFunctions();
+        $userDetails["email"] = "nik@email.com";
+        $userDetails["fullName"] = "Nik Paule";
+        $userDetails["password"] = "myPassIsNik";
+        $result = $userFunctions->register($userDetails);
+        $this->assertSame(true, $result);
+        $this->assertCount(3, $userDetails);
+    }
+
 }
