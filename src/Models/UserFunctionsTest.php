e188b0defd69badcff7403d1e785f549a97d7461
Georgi Orozov <georgiorozov@Georgis-MacBook-Pro.local>
2020-01-21 13:03:43 +0000

    First login test added

--- src/Models/UserFunctionsTest.php
+++ src/Models/UserFunctionsTest.php
@@ -4,4 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 class UserFunctionsTest extends TestCase {
 
+    public function testLogin() {
+        $userFunctions = new UserFunctions();
+        $result = $userFunctions->login("name@email.com", "name");
+        $this->assertSame(true, $result);
+    }
 }
